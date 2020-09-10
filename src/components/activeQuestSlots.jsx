6e3d90014f15beb8da4a4d9a6698d03b85910b0e
WilliamRWalsh <williamrobertsonwalsh@gmail.com>
2020-09-09 22:35:20 -0400

    active/possible quest files

---
+++ src/components/activeQuestSlots.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Accordion } from 'react-bootstrap';
+
+class ActiveQuestSlots extends Component {
+  state = {}
+  render() {
+    return (
+      <React.Fragment>
+        <h3>Active Quests</h3>
+        <Accordion>
+
+        </Accordion>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ActiveQuestSlots;
---
+++ src/components/possibleQuests.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Accordion } from 'react-bootstrap';
+
+class PossibleQuests extends Component {
+  state = {}
+  render() {
+    return (
+      <React.Fragment>
+        <h3>Available Quests</h3>
+        <Accordion>
+
+        </Accordion>
+      </React.Fragment>
+    );
+  }
+}
+
+export default PossibleQuests;
